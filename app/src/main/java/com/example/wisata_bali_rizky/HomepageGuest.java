0fabe3d173b312f43b27715e206a2bfbd9f6cdff
Rizky Surya Maulana <dastinmaq777@gmail.com>
2023-02-20 09:07:25 +0700

    Hapus data yang tidak berkaitan dengan tujuan

--- app/src/main/java/com/example/wisata_bali_rizky/HomepageGuest.java
+++ app/src/main/java/com/example/wisata_bali_rizky/HomepageGuest.java
@@ -2,7 +2,11 @@ package com.example.wisata_bali_rizky;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomepageGuest extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class HomepageGuest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_guest);
+
+        ImageView imageView = findViewById(R.id.imgMountBromo);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomepageGuest.this, MountBromoDetail.class);
+                startActivity(intent);
+            }
+        });
     }
 }
